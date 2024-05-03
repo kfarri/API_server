b0725d2bf585939d30786ffe1dd024c9d6f06004
kfarri <kwanwoo55@naver.com>
2024-05-03 10:01:45 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/sound/:name', function (req, res) {
+    const { name } = req.params
+
+    if(name == "dog"){
+        res.json({'sound' : 'mung'})
+    } else if(name == "cat"){
+        res.json({'sound' : "nyang"})
+    } else if(name == "pig"){
+        res.json({'sound' : "ggul"})
+    } else {
+        res.json({"sound" : "empty"})
+    }
+    
+})
+
+app.get('/user', function (req, res) {
+
+
+  res.send('user')
+})
+
+app.listen(80)
