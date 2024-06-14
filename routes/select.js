42e719d8d3851895a2a49f946e382a9832e35010
CoderGangW <dbsdnjs002@naver.com>
2024-06-14 14:14:44 +0900

    minor correction

--- routes/select.js
+++ routes/select.js
@@ -227,7 +227,7 @@ router.post('/web-login', (req, res) => {
 
         if (results.length > 0) {
             // 로그인 성공 시 사용자 이메일을 JSON으로 응답
-            res.status(200).json({ userEmail: results[0].ad_email });
+            res.status(200).json({ userEmail: results[0].ad_email, userName: results[0].ad_name });
         } else {
             res.status(401).send('이메일 또는 비밀번호가 틀렸습니다.');
             console.log("err");
